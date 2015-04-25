55ea67523c97f96f23d56f97d74654f6ba90a66d
Jisoo Kim <jsalmighty1989@gmail.com>
2015-04-25 22:32:07 +0900

    AEMath
    new utility class
    rad2deg , deg2rad

---
+++ src/engine/base/AEMath.java
@@ -0,0 +1,10 @@
+package engine.base;
+
+public class AEMath {
+	public static float deg2rad( float deg) {
+		return deg * 0.01745329252f;
+	}
+	public static float rad2deg( float rad) {
+		return rad * 57.295779513f;
+	}
+}
