e089a60e1822970485f428e6d5c9db226545c354
jsalmighty89 <jsalmighty1989@gmail.com>
2015-05-15 04:26:06 +0900

    AERoot
    isTypeOf  ( instanceof )

--- src/engine/base/AERoot.java
+++ src/engine/base/AERoot.java
@@ -4,4 +4,15 @@ public class AERoot {
 	public AERoot() {
 		
 	}
+	
+	public boolean isTypeOf( Class cls) {
+		Class currentClass = this.getClass();
+		while( currentClass != Object.class) {
+			if( currentClass == cls)
+				return true;
+			currentClass = currentClass.getSuperclass();
+		}
+		
+		return false;
+	}
 }
