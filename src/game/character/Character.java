2425c25b794c134246d0a1c45665fd2f22f4b352
jsalmighty89 <jsalmighty1989@gmail.com>
2015-05-15 04:28:21 +0900

    Character, Projectile
    Character
    added onTakeDamage(), initialize() methods.
    Projectile
    changed constructor.
    changed onCollide behavior

--- src/game/character/Character.java
+++ src/game/character/Character.java
@@ -3,7 +3,9 @@ package game.character;
 import org.newdawn.slick.GameContainer;
 
 import engine.base.AEVector;
+import engine.framework.AEFramework;
 import engine.object.AEGameObject;
+import game.weapon.Weapon;
 
 public class Character extends AEGameObject {
 	
@@ -19,20 +21,35 @@ public class Character extends AEGameObject {
 	
 	public Character() {
 		currentMovement = new AEVector();
+		
+		healthMax = 100.0f;
+		
+		initialize();
 	}
 	
+	
+	public void initialize() {
+		isAlive = true;
+		health = healthMax;
+	}
 	
 	public void addHealth( float health) {
 		health = Math.min( healthMax, this.health + health);		
 	}
 	public void decreaseHealth( float health) {
 		this.health -= health;
-		if( health <= 0.0f)
+		if( this.health <= 0.0f)
 			onDeath();
 	}
 
+	
+	public void onTakeDamage( Weapon weapon) {
+		decreaseHealth( weapon.getDamage());
+	}
+	
 	public void onDeath() {
 		isAlive = false;
+		AEFramework.getInstance().removeFromScene( this);
 	}
 	
 	public void update(float deltaTime, GameContainer gc) {
--- src/game/weapon/Projectile.java
+++ src/game/weapon/Projectile.java
@@ -2,21 +2,27 @@ package game.weapon;
 
 import org.newdawn.slick.GameContainer;
 
+import game.character.Character;
 import engine.base.AEVector;
 import engine.framework.AEFramework;
 import engine.object.AEGameObject;
 
 public class Projectile extends AEGameObject {
+	protected Weapon firedFrom;
+	
 	protected AEVector forward;
 	protected float lifeTime;
 	
-	public Projectile() {
+	public Projectile( Weapon firedFrom) {
+		this.firedFrom = firedFrom;
+		
 		this.setObjectName( "Projectile");
 		this.createSprite( "res/images/bullet.png");
 		this.createCollider( 10.0f);
 		
 		lifeTime = 2.0f;
 	}
+	
 	
 	public void setForward( AEVector forward) {
 		this.forward = forward;
@@ -38,7 +44,11 @@ public class Projectile extends AEGameObject {
 	@Override
 	public void onCollide(AEGameObject collider) {
 		super.onCollide(collider);
-		if( collider.getObjectName() == "Monster")
-			AEFramework.getInstance().removeFromScene( collider);
+
+		if( collider.isTypeOf( Character.class)) {
+			((Character)collider).onTakeDamage( firedFrom);
+		}
+		
+		AEFramework.getInstance().removeFromScene( this);
 	}
 }
