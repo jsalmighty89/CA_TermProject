33aa3016f9eb47b48574d333fe1a5feb2bbf35c9
ey Hwang <sc1703@naver.com>
2015-06-05 00:26:15 +0900

    Add image to launcher
    런쳐 화면 오른쪽에 플레이어에 해당하는 이미지 출력

--- src/Launcher.java
+++ src/Launcher.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.ButtonGroup;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -16,6 +17,8 @@ import javax.swing.border.EmptyBorder;
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.SlickException;
 
+import javax.swing.JLabel;
+
 class CharacterButton extends JRadioButton {
 	public int idx;
 
@@ -23,13 +26,13 @@ class CharacterButton extends JRadioButton {
 		super(text);
 		// TODO Auto-generated constructor stub
 	}
-	
 }
 
 public class Launcher extends JFrame implements ActionListener {
 
 	private JPanel contentPane;
 	private ButtonGroup btngrp;
+	private JLabel lblNewLabel;
 
 	/**
 	 * Launch the application.
@@ -54,7 +57,6 @@ public class Launcher extends JFrame implements ActionListener {
 			}
 		});
 	}
-	
 
 	/**
 	 * Create the frame.
@@ -83,31 +85,31 @@ public class Launcher extends JFrame implements ActionListener {
 		rdbtnCharacter.setBounds(12, 6, 121, 23);
 		contentPane.add(rdbtnCharacter);
 		rdbtnCharacter.idx = 0;
-		rdbtnCharacter.addActionListener( this);
+		rdbtnCharacter.addActionListener(this);
 
 		CharacterButton rdbtnCharacter_1 = new CharacterButton("Character 2");
 		rdbtnCharacter_1.setBounds(12, 31, 121, 23);
 		contentPane.add(rdbtnCharacter_1);
 		rdbtnCharacter_1.idx = 1;
-		rdbtnCharacter_1.addActionListener( this);
+		rdbtnCharacter_1.addActionListener(this);
 
 		CharacterButton rdbtnCharacter_2 = new CharacterButton("Character 3");
 		rdbtnCharacter_2.setBounds(12, 56, 121, 23);
 		contentPane.add(rdbtnCharacter_2);
 		rdbtnCharacter_2.idx = 2;
-		rdbtnCharacter_2.addActionListener( this);
+		rdbtnCharacter_2.addActionListener(this);
 
 		CharacterButton rdbtnCharacter_3 = new CharacterButton("Character 4");
 		rdbtnCharacter_3.setBounds(12, 81, 121, 23);
 		contentPane.add(rdbtnCharacter_3);
 		rdbtnCharacter_3.idx = 3;
-		rdbtnCharacter_3.addActionListener( this);
+		rdbtnCharacter_3.addActionListener(this);
 
 		CharacterButton rdbtnCharacter_4 = new CharacterButton("Character 5");
 		rdbtnCharacter_4.setBounds(12, 106, 121, 23);
 		contentPane.add(rdbtnCharacter_4);
 		rdbtnCharacter_4.idx = 4;
-		rdbtnCharacter_4.addActionListener( this);
+		rdbtnCharacter_4.addActionListener(this);
 
 		btngrp = new ButtonGroup();
 		btngrp.add(rdbtnCharacter);
@@ -115,38 +117,49 @@ public class Launcher extends JFrame implements ActionListener {
 		btngrp.add(rdbtnCharacter_2);
 		btngrp.add(rdbtnCharacter_3);
 		btngrp.add(rdbtnCharacter_4);
-		
-		
+
+		lblNewLabel = new JLabel(new ImageIcon("res/images/player.png"));
+		lblNewLabel.setBounds(224, 10, 198, 139);
+		contentPane.add(lblNewLabel);
+
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		
+
 		if (e.getActionCommand().equals("Start")) {
 			this.setVisible(false);
 			AppGameContainer app;
 			try {
-				
+
 				PlayerKJS.getDescription();
-				//GameDataManager.getInstance().setSelectedPlayerIdx(2);
-				
+				// GameDataManager.getInstance().setSelectedPlayerIdx(2);
+
 				app = new AppGameContainer(new Test("TestWindow"));
-				app.setDisplayMode( 1280, 720, false);
+				app.setDisplayMode(1280, 720, false);
 				app.start();
-				
-				
+
 			} catch (SlickException e1) {
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
 			}
-			
+
 		} else if (e.getActionCommand().equals("Exit")) {
 			System.exit(0);
-		}
-		else if( e.getSource() instanceof CharacterButton) {
-			CharacterButton btnCharacter = (CharacterButton)e.getSource();
+		} else if (e.getSource() instanceof CharacterButton) {
+			CharacterButton btnCharacter = (CharacterButton) e.getSource();
 			GameDataManager.selectedPlayerIdx = btnCharacter.idx;
+			
+			if (e.getActionCommand().equals("Character 2")) {
+				lblNewLabel.setIcon(new ImageIcon("res/images/ironman.png"));
+			} else if (e.getActionCommand().equals("Character 4")) {
+				lblNewLabel.setIcon(new ImageIcon("res/images/playerhun.png"));
+			} else if (e.getActionCommand().equals("Character 5")) {
+				lblNewLabel.setIcon(new ImageIcon("res/images/sas.png"));
+			} else {
+				lblNewLabel.setIcon(new ImageIcon("res/images/player.png"));
+			}
 		}
 	}
 }
